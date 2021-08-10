0c0ecdd20b56cd12add8940420dbf0f84100df7c
Gytis Jakutonis <g.jakutonis@gmail.com>
2021-08-10 22:49:58 +0300

    data

--- net/src/main/kotlin/gj/meteoras/net/data/PlaceNet.kt
+++ net/src/main/kotlin/gj/meteoras/net/data/PlaceNet.kt
@@ -1,11 +1,12 @@
 package gj.meteoras.net.data
 
 data class PlaceNet(
-    val code : String?,
-    val name : String?,
-    val administrativeDivision : String? = null,
-    val countryCode : String?,
-    val coordinates : Coordinates? = null
+    val code: String?,
+    val name: String?,
+    val administrativeDivision: String? = null,
+    val countryCode: String?,
+    val country: String? = null,
+    val coordinates: Coordinates? = null,
 ) {
     data class Coordinates(
         val latitude : Double?,
