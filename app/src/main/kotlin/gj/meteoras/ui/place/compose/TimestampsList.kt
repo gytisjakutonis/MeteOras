b0561ec0bce3910fa3f7bf25bd047ad09128d189
Gytis Jakutonis <g.jakutonis@gmail.com>
2021-08-17 14:57:19 +0300

    sticky header

--- app/src/main/kotlin/gj/meteoras/ui/place/compose/TimestampsList.kt
+++ app/src/main/kotlin/gj/meteoras/ui/place/compose/TimestampsList.kt
@@ -1,14 +1,20 @@
 package gj.meteoras.ui.place.compose
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.ui.Modifier
 import gj.meteoras.data.Forecast
 import java.time.ZoneId
 
+@ExperimentalFoundationApi
 @Composable
 fun TimestampsList(
     items: List<Forecast.Timestamp>
@@ -20,8 +26,10 @@ fun TimestampsList(
 
     LazyColumn(state = listState,) {
         grouped.value.forEach { (date, timestamps) ->
-            item(key = date) {
-                Day(date)
+            stickyHeader(key = date) {
+                Column(modifier = Modifier.background(MaterialTheme.colors.background)) {
+                    Day(date)
+                }
             }
 
             items(timestamps) { timestamp ->
